f394abab02e1d606e1ca8aad04e8ed973ddfd294
Manikanta662001 <gundlurimanikanta142@gmail.com>
2024-10-22 15:13:18 +0530

    added download functanality for images in chatpage

--- src/components/ChatImage.jsx
+++ src/components/ChatImage.jsx
@@ -1,11 +1,20 @@
-import { Box, Typography } from "@mui/material";
+import { Box, IconButton, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { getImageFromDb } from "../utils/utils";
+import DownloadIcon from "@mui/icons-material/Download";
 import "../pages/chatpage/ChatWindow.css";
 
 const ChatImage = ({ currentUser, singleMessage }) => {
   const { fileLink, from, time } = singleMessage;
   const [imageUrl, setImageUrl] = useState("");
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = imageUrl;
+    link.download = singleMessage.content;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
   useEffect(() => {
     const fetchImage = async () => {
       const url = await getImageFromDb(singleMessage.fileLink);
@@ -13,11 +22,15 @@ const ChatImage = ({ currentUser, singleMessage }) => {
     };
     fetchImage();
   }, [fileLink]);
+  console.log("URL::::",imageUrl,singleMessage)
   return (
     <Box className={currentUser._id === from?.id && "user-message"}>
       <Typography component={"p"}>
         <img src={imageUrl} alt="no image" />
         <span>{time}</span>
+        <IconButton onClick={handleDownload}>
+          <DownloadIcon />
+        </IconButton>
       </Typography>
     </Box>
   );
